70341c02afebb4852eee86ff6a71177bd0bb1515
Jireh Ng <jirehng123@gmail.com>
2025-04-13 20:47:55 -0400

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,65 @@
+const projects = [
+  {
+    title: "The Weather Project",
+    description: "A dynamic weather website using Openweather API to show the local current weather forecast for any city",
+    images: "Images/weatherapp.png",
+    languages: "HTML, CSS, Javascript",
+    link: "https://jirehng.github.io/TheWeatherProject/", 
+    color: "js",
+    type: "web",
+    status: "completed",
+  },
+
+  {
+    title: "Mini Todo",
+    description: "A simple todo-list application that allow user to add new task, complete existing tasks, and delete unwanted tasks",
+    images: "Images/todolist.png",
+    languages: "HTML, CSS, React.js",
+    link: "https://github.com/jirehng/mini-todolist", 
+    color: "reactjs",
+    type: "web",
+    status: "completed",
+  },
+  {
+    title: "Jaggurnaut Assault",
+    description: "A two player PVP tank game made for grade 11 computer science final project.",
+    images: "Images/juggernaut assault.png",
+    languages: "Java, Java Graphics",
+    color: "javaC",
+    link: "https://github.com/jirehng/Juggernaut-Assault",
+    type: "web",
+    status: "completed",
+
+  }, 
+  {
+    title: "StudyGrate",
+    description: "All in one study app that allows students to access study tools and materials and connect with other students",
+    images:"Images/studygrate.png",
+    languages: "Figma, UI/UX design",
+    color: "uiuxC",
+    type: "design",
+    status: "wip",
+    link: "https://www.figma.com/design/9VoK1fpIz5jX6kGFcSPViQ/StudyGrate---YRHacks-Project?node-id=0-1&t=zZgBy3U2P5nokKQa-1",
+  }, 
+  {
+    title: "Re:Wake (WIP)",
+    description: "An iOS alarm clock app that approaches alarm in a brand new way. Hence the name Re(imagine):Wake",
+    images: "Images/WIP.jpg",
+    languages: "Swift, SwiftUI",
+    color: "swiftC",
+    type: "mobile",
+    status: "wip",
+
+  },
+
+  {
+    title: "HSPrep (WIP)",
+    description: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget conse",
+    images: "Images/WIP.jpg",
+    languages: "HTMl, CSS, React.js",
+    color: "reactjs",
+    type: "web",
+    status: "wip",
+
+  },
+]
--- script.js
+++ script.js
@@ -13,3 +13,92 @@ document.querySelector(".projects").onmousemove = e => {
 function toClick(link) {
   window.location.href = link;
 }
+
+
+
+
+const projectContainer = document.querySelector(".projects");
+const menuButton = document.querySelectorAll(".menuButton");
+let currentFilter = "all";
+
+let filteredProjects;
+
+function renderProjects(element) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.classList.add(element.color);
+
+  card.addEventListener("click", () => {
+    window.open(element.link, "_blank");
+  });
+
+  const cardContent = document.createElement("div");
+  cardContent.classList.add("cardContent");
+  card.append(cardContent);
+
+  const image = document.createElement("img");
+  image.src = element.images;
+  cardContent.append(image);
+
+  const title = document.createElement("h3");
+  title.classList.add("projectTitle");
+  title.textContent = element.title;
+  cardContent.append(title);
+
+  const languages = document.createElement("p");
+  languages.classList.add("languages");
+  languages.textContent = element.languages;
+  cardContent.append(languages);
+
+  const description = document.createElement("p");
+  description.classList.add("projectDescription");
+  description.textContent = element.description;
+  cardContent.append(description);
+
+  return card;
+}
+
+
+function menuSelect(className) {
+  if(className == ".all") currentFilter = "all"
+  else if(className == ".web") currentFilter = "web"
+  else if(className == ".mobile") currentFilter = "mobile"
+  else if(className == ".uiux") currentFilter = "design"
+  document.querySelectorAll(".selected").forEach(element => {
+    element.classList.remove("selected");
+  })
+  document.querySelectorAll(className).forEach(element => {
+    element.classList.add("selected");
+  });
+}
+
+if(currentFilter === "all") {
+  if (currentFilter === "all") {
+    filteredProjects = projects;
+  }
+
+  filteredProjects.forEach(element => {
+    const card = renderProjects(element);
+    projectContainer.append(card);
+  });
+}
+
+menuButton.forEach(button => {
+  button.addEventListener("click", (e) => {
+    projectContainer.innerHTML = "";
+    if (currentFilter === "all") {
+      filteredProjects = projects;
+    } else if (currentFilter === "web") {
+      filteredProjects = projects.filter(element => element.type === "web");
+    } else if (currentFilter === "mobile") {
+      filteredProjects = projects.filter(element => element.type === "mobile");
+    } else if (currentFilter === "design") {
+      filteredProjects = projects.filter(element => element.type === "design");
+    }
+
+    filteredProjects.forEach(element => {
+      const card = renderProjects(element);
+      projectContainer.append(card);
+    });
+  });
+});
