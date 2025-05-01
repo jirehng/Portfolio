0740fc27dc0c02c5edc8065503b7a8372f3189e1
Jireh Ng <jirehng123@gmail.com>
2025-04-30 20:48:09 -0400

    Update projects.js

--- projects.js
+++ projects.js
@@ -54,7 +54,7 @@ const projects = [
 
   {
     title: "HSPrep (WIP)",
-    description: "A centralize website where Ontario high school students can post and study other students' notes, past tests, practice problems, and materials",
+    description: "A centralize website where Ontario high school students can post and study other students' notes, past tests, and course materials",
     images: "Images/WIP.jpg",
     languages: "HTMl, CSS, React.js",
     color: "reactjs",
